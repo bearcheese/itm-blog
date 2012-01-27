65cfda67b8aefd8a28a5ac57e22048a3c974e162
bearcheese <bearcheese@gmail.com>
2012-01-27 19:59:20 +0100

    Initial project layout.

---
+++ itm-java/itm-common/src/main/java/hu/bearmaster/itm/common/App.java
@@ -0,0 +1,13 @@
+package hu.bearmaster.itm.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
